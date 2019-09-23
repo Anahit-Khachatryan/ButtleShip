841a6e8e82debecc46cd90bc3bc2927cf8818db4
Ani Khachatryan <anahitxachatryan11@gmail.com>
2019-09-24 00:30:45 +0400

    Create ships

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var squareSize = 50;
 
 // get the container element
 var gameBoardContainer = document.getElementById("gameboard");
-
 // make the grid columns and rows
 for (i = 0; i < cols; i++) {
 	for (j = 0; j < rows; j++) {
@@ -18,89 +17,28 @@ for (i = 0; i < cols; i++) {
 		
 		// set each grid square's coordinates: multiples of the current row or column number
         var topPosition = j * squareSize;
-        console.log(topPosition);
 		var leftPosition = i * squareSize;			
-		console.log(leftPosition);
 		// use CSS absolute positioning to place each grid square on the page
 		square.style.top = topPosition + 'px';
 		square.style.left = leftPosition + 'px';						
 	}
 }
-var ship = document.getElementById('ships');
-var row = 1;
-var row2 = 2;
-for(let i = 0; i < row2; i++){
+$( function() {
+  $( "#draggable" ).draggable();
+} );
+var ship = document.getElementById("ship");
+console.log(ship);
+
+var row2 = 5;
+for(let i = 1; i < row2; i++){
     let square = document.createElement("div");
     ship.appendChild(square);
-    ship.id = 's'+i;
-    let leftPosition = i*squareSize;
-    console.log(leftPosition);
-    ship.style.left = leftPosition + 'px';
+    // square.id = 's'+i;
+    // let leftPosition = i*squareSize;
+    // // console.log(leftPosition);
+    // square.style.left = leftPosition + 'px';
+    let leftwidth = i * squareSize;
+    square.style.width = leftwidth + "px";
+    // square.setAttribute("draggable", ID)
+
 }
-
-/* lazy way of tracking when the game is won: just increment hitCount on every hit
-   in this version, and according to the official Hasbro rules (http://www.hasbro.com/common/instruct/BattleShip_(2002).PDF)
-   there are 17 hits to be made in order to win the game:
-      Carrier     - 5 hits
-      Battleship  - 4 hits
-      Destroyer   - 3 hits
-      Submarine   - 3 hits
-      Patrol Boat - 2 hits
-*/
-var hitCount = 0;
-
-/* create the 2d array that will contain the status of each square on the board
-   and place ships on the board (later, create function for random placement!)
-   0 = empty, 1 = part of a ship, 2 = a sunken part of a ship, 3 = a missed shot
-*/
-var gameBoard = [
-				[0,0,0,1,1,1,1,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,1,0,0,0],
-				[0,0,0,0,0,0,1,0,0,0],
-				[1,0,0,0,0,0,1,1,1,1],
-				[1,0,0,0,0,0,0,0,0,0],
-				[1,0,0,1,0,0,0,0,0,0],
-				[1,0,0,1,0,0,0,0,0,0],
-				[1,0,0,0,0,0,0,0,0,0]
-				]
-
-// set event listener for all elements in gameboard, run fireTorpedo function when square is clicked
-gameBoardContainer.addEventListener("click", fireTorpedo, false);
-
-// initial code via http://www.kirupa.com/html5/handling_events_for_many_elements.htm:
-// function fireTorpedo(e) {
-//     // if item clicked (e.target) is not the parent element on which the event listener was set (e.currentTarget)
-// 	if (e.target !== e.currentTarget) {
-//         // extract row and column # from the HTML element's id
-// 		var row = e.target.id.substring(1,2);
-// 		var col = e.target.id.substring(2,3);
-//         //alert("Clicked on row " + row + ", col " + col);
-				
-// 		// if player clicks a square with no ship, change the color and change square's value
-// 		if (gameBoard[row][col] == 0) {
-// 			e.target.style.background = '#bbb';
-// 			// set this square's value to 3 to indicate that they fired and missed
-// 			gameBoard[row][col] = 3;
-			
-// 		// if player clicks a square with a ship, change the color and change square's value
-// 		} else if (gameBoard[row][col] == 1) {
-// 			e.target.style.background = 'red';
-// 			// set this square's value to 2 to indicate the ship has been hit
-// 			gameBoard[row][col] = 2;
-			
-// 			// increment hitCount each time a ship is hit
-// 			hitCount++;
-// 			// this definitely shouldn't be hard-coded, but here it is anyway. lazy, simple solution:
-// 			if (hitCount == 17) {
-// 				alert("All enemy battleships have been defeated! You win!");
-// 			}
-			
-// 		// if player clicks a square that's been previously hit, let them know
-// 		} else if (gameBoard[row][col] > 1) {
-// 			alert("Stop wasting your torpedos! You already fired at this location.");
-// 		}		
-//     }
-//     e.stopPropagation();
-// }
